f57566996efdecb91443d6e4198a5d8a59e83f70
David Vinicius Da Silva <ht3000052@HTO.IFSP.EDU.BR>
2018-05-08 23:09:54 -0800

    Alterando metodos em TALK DAO

--- src/MODEL/TALKDAO/TalkDAO.java
+++ src/MODEL/TALKDAO/TalkDAO.java
@@ -9,12 +9,18 @@ import Services.SQLiteConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author David
  */
 public class TalkDAO implements ITalkDAO{
+    
+    public Talk talk;
 
     @Override
     public Talk getTalkWhere(String name) {
@@ -46,8 +52,39 @@ public class TalkDAO implements ITalkDAO{
 
     @Override
     public Talk[] getTalks() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        Connection con   = null;
+        ResultSet  rs    = null;
+        PreparedStatement stm    = null;
+        Talk[]    talks  = null;
+        
+        
+        try {
+            con = SQLiteConnection.getConnection();            
+            stm = con.prepareStatement("SELECT * FROM Talks");
+            rs  = stm.executeQuery();
+            
+            talks = new Talk[this.size()];
+                        
+            int i = 0;
+            while(rs.next())
+            {
+                talk = new Talk();
+                talk.setId(rs.getInt("id"));
+                talk.setName(rs.getString("name"));
+                talks[i] = talk;
+                i++;
+            }
+            
+            return talks;
+        } catch (SQLException ex) {
+            Logger.getLogger(TalkDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        finally{
+            SQLiteConnection.closeConnection(con);
+        }
+        return null;
     }
+        
 
     @Override
     public boolean createTalk(Talk talk) {
@@ -57,6 +94,33 @@ public class TalkDAO implements ITalkDAO{
     @Override
     public boolean deleteTalk(Talk talk) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    public int size() throws SQLException
+    {
+        Connection        con = null;
+        PreparedStatement stm = null;
+        ResultSet         rs  = null;
+        int total             = 0;
+        
+        try{
+            con = SQLiteConnection.getConnection();
+            stm = con.prepareStatement("SELECT count(*) as rows FROM Talks");
+            rs  = stm.executeQuery();
+            
+            
+            while(rs.next())
+            {
+                total = rs.getInt("rows");
+            }
+            
+            return total;
+            
+        }catch(Exception e)
+        {
+            System.out.println(e);
+        }
+        return total;
     }
     
 }
